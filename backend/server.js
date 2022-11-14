82d394e17b860549dad8684e25c3fd981e196e74
saravanan senthil <sarusenth@gmail.com>
2022-11-14 13:51:44 +0530

    slides

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
+const pdf2pic = require("pdf2pic");
 const express = require('express')
 const cors = require('cors')
+const pdf = require('pdf-page-counter');
 const bodyParser = require('body-parser')
 const app = express()
 const port = 4000
 const fs = require('fs')
+var pageNumber = 1;
+
 app.use(cors())
-app.use(bodyParser.json({limit: '200mb'}));
+app.use(bodyParser.json({ limit: '200mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -14,20 +18,77 @@ app.get('/', (req, res) => {
 function dataURLtoFile(data, filename) {
   var base64Data = data.replace(/^data:image\/png;base64,/, "");
 
-  require("fs").writeFile(filename, base64Data, 'base64', function(err) {
-    console.log(err);  })
+  require("fs").writeFile(filename, base64Data, 'base64', function (err) {
+    console.log(err);
+  })
 }
 
+function pdftopic(pdfPath) {
+  console.log('hi')
+  let dataBuffer = fs.readFileSync(pdfPath);
+  pdf(dataBuffer).then(function (data) {
+    var numPages = data.numpages;
+    console.log(data)
+    const options = {
+      density: 100,
+      saveFilename: "slides",
+      savePath: "./images",
+      format: "png",
+      width: 600,
+      height: 600
+    };
+    const storeAsImage = pdf2pic.fromPath(pdfPath, options);
+
+    for (let i = 1; i <= numPages; i++) {
+      storeAsImage(i).then((resolve) => {
+        console.log(resolve); // send resolve to user
+      });
+      // pdf2pic.fromBase64(pdfData, options).convert(i, isBase64)
+    }
+  })
+}
+
+
 app.post('/saveImg', (req, res) => {
-    // console.log(req.body.imData)
-    dataURLtoFile(req.body.imData, 'canvas.png')
-    return res.json({msg: 'recieved!'})
+  // console.log(req.body.imData)
+  dataURLtoFile(req.body.imData, 'canvas.png')
+  return res.json({ msg: 'recieved!' })
+})
+
+app.post('/savePdf', (req, res) => {
+  // console.log(req.body.imData)
+  pdfData = req.body.pdf.split(',')[1];
+  // console.log(pdfData)
+  var binary = atob(pdfData)
+  fs.writeFileSync('slides.pdf', binary, 'binary')
+  pdftopic('slides.pdf')
+  // pdftopic(pdfData, 10);
+  return res.json({ msg: 'recieved!' })
 })
 
 
 app.get('/img', (req, res) => {
 
   res.sendFile('/Users/saru/arvr/whiteboard/backend/canvas.png');
+})
+
+app.get('/slide', (req, res) => {
+  pageNo = req.query.pageNo;
+  if (Object.keys(req.query).length !== 0){
+  res.sendFile(`/Users/saru/arvr/whiteboard/backend/images/slides.${pageNo}.png`)}
+  else {
+    res.sendFile(`/Users/saru/arvr/whiteboard/backend/images/slides.${pageNumber}.png`)}
+  }
+)
+
+app.post('/setSlide', (req, res)=> {
+  pageNumber = req.body.pageNo
+  console.log('hey')
+  res.json({msg:'done'})
+})
+
+app.get('/thing', (req, res) => {
+  pdftopic();
 })
 
 app.listen(port, () => {
--- paint-app/src/App.js
+++ paint-app/src/App.js
@@ -2,7 +2,8 @@ import { useEffect, useRef, useState } from "react";
 import Menu from "./components/Menu";
 import "./App.css";
 import axios from 'axios';
-  
+import  { useDropzone } from 'react-dropzone';
+
 function App() {
   const canvasRef = useRef(null);
   const ctxRef = useRef(null);
@@ -10,7 +11,18 @@ function App() {
   const [lineWidth, setLineWidth] = useState(5);
   const [lineColor, setLineColor] = useState("black");
   const [lineOpacity, setLineOpacity] = useState(0.1);
-  
+  const [files, setFiles] = useState(null)
+  const [pageNo, setPageNo] = useState(1)
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "pdf/*",
+    onDrop: (acceptedFiles) => {
+      console.log(acceptedFiles)
+      setFiles(acceptedFiles)
+      
+    },
+  })
+
   // Initialization when the component
   // mounts for the first time
   useEffect(() => {
@@ -23,44 +35,96 @@ function App() {
     ctx.lineWidth = lineWidth;
     ctxRef.current = ctx;
   }, [lineColor, lineOpacity, lineWidth]);
-  
+
+  useEffect(() => {
+    console.log('hey')
+    if (files == null) {
+      console.log('no file')
+      return
+    }
+    console.log(files[0])
+    let reader = new FileReader();
+    reader.readAsDataURL(files[0]);
+    reader.onloadend = (e) => {
+      axios.post('http://localhost:4000/savePdf', { pdf: e.target.result }).then(res => {
+        console.log(res)
+      })
+    };
+
+  }, [files])
+
+
   // Function for starting the drawing
   const startDrawing = (e) => {
     ctxRef.current.beginPath();
     ctxRef.current.moveTo(
-      e.nativeEvent.offsetX, 
+      e.nativeEvent.offsetX,
       e.nativeEvent.offsetY
     );
     setIsDrawing(true);
   };
-  
+
   // Function for ending the drawing
   const endDrawing = () => {
     saveToBackend();
     ctxRef.current.closePath();
     setIsDrawing(false);
   };
-  
+
   const saveToBackend = (e) => {
     const canvas = document.getElementById('toSave');
     const img = canvas.toDataURL('image/png')
-    axios.post('http://localhost:4000/saveImg', {imData: img}).then(res => {
+    axios.post('http://localhost:4000/saveImg', { imData: img }).then(res => {
       console.log(res)
-  })
+    })
   }
+
+  // const uploadSlides = (uploadSlides) => {
+  //   console.log('hey')
+  //   if (files == null) {
+  //     console.log('no file')
+  //     return
+  //   }
+  //   console.log(uploadSlides)
+  //   let reader = new FileReader();
+  //   reader.readAsDataURL(uploadSlides);
+  //   reader.onloadend = (e) => {
+  //     axios.post('http://localhost:4000/savePdf', { pdf: e.target.result }).then(res => {
+  //       console.log(res)
+  //     })
+  //   };
+
+  // }
 
   const draw = (e) => {
     if (!isDrawing) {
       return;
     }
     ctxRef.current.lineTo(
-      e.nativeEvent.offsetX, 
+      e.nativeEvent.offsetX,
       e.nativeEvent.offsetY
     );
-      
+
     ctxRef.current.stroke();
   };
-  
+
+  const dropzoneText = () => {
+    if (files == null) {
+      return "drop file here";
+    }
+    else return files[0].name
+  }
+
+  const handleSubmit = (e) => {
+    axios.post('http://localhost:4000/setSlide', {pageNo: pageNo}).then(res => {
+      console.log(res)
+    })
+  }
+
+  const handleChange = (e) => {
+    setPageNo(e.target.value)
+  }
+
   return (
     <div className="App">
       <h1>Paint App</h1>
@@ -71,7 +135,7 @@ function App() {
           setLineOpacity={setLineOpacity}
         />
         <canvas
-          id= "toSave"
+          id="toSave"
           onMouseDown={startDrawing}
           onMouseUp={endDrawing}
           onMouseMove={draw}
@@ -81,9 +145,19 @@ function App() {
         />
         <button onClick={saveToBackend}> save
         </button>
+
+        <div {...getRootProps()}>
+          <input {...getInputProps()} />
+          {dropzoneText()}
+        </div>
+          <label>
+            Name:
+            <input type="text" value={pageNo} onChange={handleChange} />
+          </label>
+          <input type="button" value="apply" onClick={handleSubmit} />
       </div>
     </div>
   );
 }
-  
+
 export default App;
